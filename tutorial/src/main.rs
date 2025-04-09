3ccbf5de0b1d4404f7af3c5a9c797e4e813ab347
0x-sxmeer <20sameer90@gmail.com>
2025-04-10 00:48:03 +0530

    b

--- tutorial/src/main.rs
+++ tutorial/src/main.rs
@@ -8,6 +8,13 @@ fn main() {
     println!("valve of x is {}", x);
     println!("valve of y is {}", y);
 
+    {
+        let x = 10;
+        println!("valve of x is {}", x);
+    }
+
+
+
     x = 5;
     println!("valve of x is {}", x);
 }
