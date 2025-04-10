f616da29f00138ff5c01fc9254ccf58590f0d07c
0x-sxmeer <20sameer90@gmail.com>
2025-04-10 12:48:27 +0530

    h

--- tutorial/src/main.rs
+++ tutorial/src/main.rs
@@ -1,20 +1,23 @@
 fn main() {
     println!("Hello, peter");
 
-    let mut x = 5;
+
+    let x = 5;
     println!("valve of x is {}", x);
 
    let y = 6;
-    println!("valve of x is {}", x);
-    println!("valve of y is {}", y);
+   println!("valve of y is {}", y);
 
     {
         let x = 10;
         println!("valve of x is {}", x);
     }
 
+    let y = x+2;
+    println!("valve of new x {}", y);
+
+    let y= "hello world";
+    print!("valve of h is {}", y);
 
 
-    x = 5;
-    println!("valve of x is {}", x);
 }
