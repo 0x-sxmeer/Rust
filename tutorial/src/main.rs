9e5e7dd7900d7517c9553ad095853191a0e00557
0x-sxmeer <20sameer90@gmail.com>
2025-04-18 01:30:26 +0530

    Add incremental compilation artifacts for tutorial project
    - Created new binary files for query cache and work products.
    - Added lock file for incremental compilation state.

--- tutorial/src/main.rs
+++ tutorial/src/main.rs
@@ -1,9 +1,33 @@
-
 fn main() {
     println!("Hello, peter");
+    another_function(42,'a');
+}
 
 
 
+//Parameters and Arguments
+fn another_function(num: i32,letter: char) {
+    println!("The value of num is: {num}");
+    println!("The value of letter is: {letter}");
+
+//Statements and Expressions
+    // -Statements are instructions that perform some action and do not return a value.
+    // -Expressions evaluate to a value and do not include a semicolon at the end.
+
+    let x = 5; // This is a statement, so it needs a semicolon
+    let y = {
+        let x = 3;
+        x + 1 // This is an expression, so it doesn't need a semicolon
+    };
+    println!("The value of y is: {y}");
+    println!("The value of x is: {x}");
 
 
 }
+
+
+//Function with Return Value
+fn five() -> i32 {
+    5 // This is an expression, so it doesn't need a semicolon
+}
+
