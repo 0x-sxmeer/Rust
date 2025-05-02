0d2d961ad5dfed973d1564bc305f885b05324a6a
0x-sxmeer <20sameer90@gmail.com>
2025-05-02 11:18:29 +0530

    Add incremental compilation artifacts for tutorial project
    - Created new binary files: `query-cache.bin`, `work-products.bin`, and `lock` file in the incremental compilation directory.
    - These files contain metadata and state information for the Rust compiler version 1.86.0, aiding in faster compilation times.

--- tutorial/src/main.rs
+++ tutorial/src/main.rs
@@ -49,6 +49,60 @@ fn main() {
 
 
 
+    let a = 10;
+    let b = 5;
+    let c = 20;
+
+    if a > b && a < c {
+        println!("a is greater than b and less than c");
+    } else if a < b || a > c {
+        println!("a is either less than b or greater than c");
+    } else {
+        println!("a is neither greater than b nor less than c");
+    }
+
+
+
+    // Match Statement in enum
+
+    enum Coin {
+        Penny,
+        Nickel,
+        Dime,
+        Quarter,
+    }
+
+    fn value_in_cents(coin: Coin) -> u32 {
+        match coin {
+            Coin::Penny => 1,
+            Coin::Nickel => 5,
+            Coin::Dime => 10,
+            Coin::Quarter => 25,
+        }
+    }
+
+    //define a coin variable
+    let my_coin = Coin::Dime;
+    let my_coin2 = Coin::Nickel;
+    let my_coin3 = Coin::Quarter;
+    let my_coin4 = Coin::Penny;
+
+    //Print the value of the coin
+    println!("The value of my_coin is: {} cents", value_in_cents(my_coin));
+    println!("The value of my_coin2 is: {} cents", value_in_cents(my_coin2));
+    println!("The value of my_coin3 is: {} cents", value_in_cents(my_coin3));
+    println!("The value of my_coin4 is: {} cents", value_in_cents(my_coin4));
+    
+
+    //Loops
+    loop {
+        println!("This loop will run forever");
+        break; // This will exit the loop
+    }
+
+    
+    
+
 
 
 }
